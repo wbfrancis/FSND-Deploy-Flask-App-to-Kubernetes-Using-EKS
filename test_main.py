8d683f4cf2c3bec51964dbd5979e789e359dd646
Will Francis <wbf7@duke.edu>
2020-06-22 16:18:42 -0500

    tests

--- test_main.py
+++ test_main.py
@@ -24,8 +24,8 @@ def client():
 
 def test_health(client):
     response = client.get('/')
-    # assert response.status_code == 200
-    # assert response.json == 'Healthy'
+    assert response.status_code == 200
+    assert response.json == 'Healthy'
     assert False
 
 
